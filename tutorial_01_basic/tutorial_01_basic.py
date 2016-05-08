4842be268e5b23711649dccf488817e878997a23
ByeongCheol Nam <lezo524@gmail.com>
2016-05-09 00:11:39 +0900

    add int and float tutorial

---
+++ tutorial_01_basic/tutorial_01_basic.py
@@ -0,0 +1,32 @@
+
+# this is the first comment
+spam = 1 # and this is the second comment
+         # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+print(spam)
+print(text)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+
+# 인터렉티브 모드에서 밑줄(_)은 가장 마지막에 출력된 값을 보관하는 변수이다.
+# print(price + _)
+# 결과는 113.0625 가 나온다.
+
+print(round(price, 2))
+
+#Built-in Functions
+# https://docs.python.org/3/library/functions.html
+
+
+
+# NUMBERS
+print("\n - NUMBERS - ")
+print(2+2)      # int
+print(50-5+6)   # int
+# / 나누기는 항상 float 리턴
+print((50-5*6)/4)   # float
+print(8/5)          # float
+
